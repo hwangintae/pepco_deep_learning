6c5416acff12a5a23b69e0fb2290d7312f533c69
int9029 <hwang9029@naver.com>
2018-07-24 15:55:37 +0900

    add PepeatReplacer & wordnet.synsets

--- 0. jarvis/3. nlp/replacers.py
+++ 0. jarvis/3. nlp/replacers.py
@@ -23,3 +23,16 @@ class RegexpReplacer(object):
         for (pattern, repl) in self.patterns:
             (s, count) = re.subn(pattern, repl, s)
         return s
+
+class RepeatReplacer(object):
+    def __init__(self):
+        self.repeat_regexp = re.compile(r'(\w*)(\w)\2(\w*)')
+        self.repl = r'\1\2\3'
+    def replace(self, word):
+        if wordnet.synsets(word):
+            return word
+        repl_word = self.repeat_regexp.sub(self.repl, word)
+        if repl_word != word:
+            return self.replace(repl_word)
+        else:
+            return repl_word
